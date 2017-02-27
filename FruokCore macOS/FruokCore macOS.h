2dabb73b3006ae0d45f077fc7ee48378867f3a5c
Sergio Daniel Lozano <zheref@icloud.com>
2017-02-27 11:50:26 -0500

    Initial commit

---
+++ FruokCore macOS/FruokCore macOS.h
@@ -0,0 +1,19 @@
+//
+//  FruokCore macOS.h
+//  FruokCore macOS
+//
+//  Created by Sergio Daniel L. García on 2/27/17.
+//  Copyright © 2017 Sergio Daniel L. García. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FruokCore macOS.
+FOUNDATION_EXPORT double FruokCore_macOSVersionNumber;
+
+//! Project version string for FruokCore macOS.
+FOUNDATION_EXPORT const unsigned char FruokCore_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FruokCore_macOS/PublicHeader.h>
+
+
---
+++ FruokCore/FruokCore.h
@@ -0,0 +1,19 @@
+//
+//  FruokCore.h
+//  FruokCore
+//
+//  Created by Sergio Daniel L. García on 2/27/17.
+//  Copyright © 2017 Sergio Daniel L. García. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FruokCore.
+FOUNDATION_EXPORT double FruokCoreVersionNumber;
+
+//! Project version string for FruokCore.
+FOUNDATION_EXPORT const unsigned char FruokCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FruokCore/PublicHeader.h>
+
+
