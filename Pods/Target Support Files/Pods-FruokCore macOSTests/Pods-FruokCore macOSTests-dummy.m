be434b7abd2a8dc9ed3736486562c8c1aacf6f1b
Sergio Daniel Lozano <zheref@icloud.com>
2017-03-09 17:00:13 -0500

    First pod install

---
+++ Pods/Target Support Files/Pods-FruokCore macOSTests/Pods-FruokCore macOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FruokCore_macOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_FruokCore_macOSTests
+@end
---
+++ Pods/Target Support Files/Pods-FruokCore/Pods-FruokCore-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FruokCore : NSObject
+@end
+@implementation PodsDummy_Pods_FruokCore
+@end
