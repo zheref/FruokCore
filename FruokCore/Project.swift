df2c9df89ede5cdd053975ae99678aaf21025ae0
Sergio Daniel Lozano <zheref@icloud.com>
2017-03-09 16:57:26 -0500

    Add project model struct

---
+++ FruokCore/Project.swift
@@ -0,0 +1,15 @@
+//
+//  Project.swift
+//  FruokCore
+//
+//  Created by Sergio Daniel L. García on 2/28/17.
+//  Copyright © 2017 Sergio Daniel L. García. All rights reserved.
+//
+
+import Foundation
+
+public struct Project {
+    
+    
+    
+}
